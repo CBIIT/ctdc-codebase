2deba0e480d59ebc95597028822204d253fb5381
jonkiky <jonkiky@gmail.com>
2020-03-23 17:03:15 -0400

    fix the font is not consistent in the pubmediD widget

--- src/main/frontend/src/components/Widgets/PieCharts/CustomActiveDonut/CustomActiveDonutView.js
+++ src/main/frontend/src/components/Widgets/PieCharts/CustomActiveDonut/CustomActiveDonutView.js
@@ -42,7 +42,7 @@ const renderActiveShape = (props) => {
 
   return (
     <g>
-      <text y={9} fill={textColor} fontSize={fontSize || '12px'} fontWeight="600" fontFamily="Raleway">{String(payload.name).length > 23 ? `${String(payload.name).substr(0, 23)}...` : payload.name}</text>
+      <text y={9} fill={textColor} fontSize={fontSize || '12px'} fontWeight="600" fontFamily="Lato">{String(payload.name).length > 23 ? `${String(payload.name).substr(0, 23)}...` : payload.name}</text>
       <text x={cx} y={cy} dy={0} textAnchor="middle" fill={textColor} fontSize="12px" fontWeight="500" fontFamily="Open Sans">{`${value}`}</text>
       <text x={cx} y={cy} dy={12} textAnchor="middle" fill={textColor} fontSize="12px" fontWeight="500" fontFamily="Open Sans">Cases</text>
       <Sector
