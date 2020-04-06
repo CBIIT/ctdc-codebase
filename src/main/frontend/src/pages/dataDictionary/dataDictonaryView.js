69db53f3036ba7034175464272ca1db6460e1ae8
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-04-06 09:07:14 -0400

    styling to DD page

--- src/main/frontend/src/pages/dataDictionary/dataDictonaryView.js
+++ src/main/frontend/src/pages/dataDictionary/dataDictonaryView.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React from 'react';
 // import { Grid, withStyles, Link } from '@material-ui/core';
 import { withStyles, Divider } from '@material-ui/core';
@@ -25,16 +23,59 @@ const AboutBody = ({ classes, data }) => (
       <Divider variant="middle" classes={{ root: classes.dividerRoot }} />
       <div className={classes.sectionContainer}>
         <div className={classes.sectionTitle}>CTDC Node Types</div>
-        <div className={classes.nodeTypeTitle}>clinical_trial</div>
-        <div className={classes.nodeTypeDesc}>An interventional clinical research study that is represented within the CTDC, in terms of its design, data and key results. </div>
-        <div className={classes.nodeTypeTitle}>arm</div>
-        <div className={classes.nodeTypeDesc}> A treatment arm tests a single therapeutic agent against a set of genomic aberrations. Each arm may accrue at most 35 patients; for arms studying more common genomic aberrations up to 70 patients may be enrolled. Each treatment arm has a set of inclusion and exclusion criteria.</div>
+        { data.nodeTypes && data.nodeTypes.map((nodeType) => (
+          <div className={classes.nodeTypeContainer}>
+            <div className={classes.numberCircle}>{nodeType.nodeTypeNUmber}</div>
+            <div className={classes.nodeTypeTextContainer}>
+              <div className={classes.nodeTypeTitle}>{nodeType.nodeTypeTitle}</div>
+              <div className={classes.nodeTypeDesc}>{nodeType.nodeTypeDescription}</div>
+              <div className={classes.nodeTypeDesc}>{nodeType.nodeTypeDescriptionParagraphTwo}</div>
+              {nodeType.nodeTypeSubList
+                && (nodeType.nodeTypeSubList.map((sublist) => (
+                  <div className={classes.nodeTypeSubListContainer}>
+                    <div className={classes.nodeTypeTitle}>{sublist.nodeTypeSubListTitle}</div>
+                    <div className={classes.nodeTypeDesc}>{sublist.nodeTypeSubListDescription}</div>
+                  </div>
+                ))
+                )}
+            </div>
+          </div>
+        ))}
+
+        {data.nodeTypeRelationShipTable && (
+        <div className={classes.tableDiv}>
+          <table className={classes.table}>
+            <thead className={classes.tableHeader}>
+              <tr className={classes.tableBodyRow}>
+                <th className={classes.headerCell} aria-label="Index" />
+                { data.nodeTypeRelationShipTable[0].head.map((rowObj) => (
+                  <>
+                    <th className={classes.headerCell}>{rowObj}</th>
+                  </>
+                )) }
+              </tr>
+            </thead>
+            <tbody>
+              { data.nodeTypeRelationShipTable[1].body.map((rowObj, index) => (
+                <>
+                  <tr className={classes.tableBodyRow}>
+                    <td className={classes.tableCell}>{index + 1}</td>
+                    {/* eslint-disable-next-line max-len */}
+                    { rowObj.row.map((rowValue) => <td className={classes.tableCell}>{rowValue}</td>)}
+                  </tr>
+                </>
+              )) }
+            </tbody>
+          </table>
+        </div>
+        )}
+
       </div>
       <Divider variant="middle" classes={{ root: classes.dividerRoot }} />
       <div className={classes.sectionContainer}>
         <div className={classes.sectionTitle}>Document Conventions</div>
         <div className={classes.nodeTypeTitle}>Descriptors for Each Attribute</div>
-        <div className={classes.nodeTypeDesc}>An interventional clinical research study that is represented within the CTDC, in terms of its design, data and key results. </div>
+        <div className={classes.nodeTypeDesc}>An interventional clinical r</div>
       </div>
       <Divider variant="middle" classes={{ root: classes.dividerRoot }} />
       <Navbar />
@@ -50,7 +91,7 @@ const AboutBody = ({ classes, data }) => (
   </>
 );
 
-const styles = () => ({
+const styles = (theme) => ({
 
   container: {
     margin: 'auto',
@@ -88,10 +129,70 @@ const styles = () => ({
   },
   nodeTypeDesc: {
     color: 'black',
-    fontFamily: 'Lato',
-    fontSize: 21,
+    fontFamily: 'Raleway',
+    fontSize: '1em',
+    fontWeight: 500,
+    lineHeight: '1.5em',
+    marginTop: '8px',
   },
+  numberCircle: {
+    float: 'left',
+    borderRadius: '50%',
+    width: 36,
+    height: 36,
+    padding: 8,
+    background: '#3FB0D5',
+    color: '#fff',
+    fontWeight: 700,
+    textAlign: 'center',
+    fontFamily: 'Lato',
+  },
+  nodeTypeTextContainer: {
+    marginLeft: '50px',
+    marginTop: '8px',
+  },
+  nodeTypeContainer: {
+    margin: '8px',
+  },
+  nodeTypeSubListContainer: {
+    marginLeft: '80px',
+  },
+  tableDiv: {
+    marginTop: '45px',
+  },
+  table: {
+    borderSpacing: '0',
+    borderCollapse: 'collapse',
+    fontSize: '12px',
+    fontWeight: 'bold',
+    letterSpacing: '0.025em',
+    lineHeight: '30px',
+    textAlign: 'left',
+    width: '100%',
+  },
+  tableHeader: {
+    fontFamily: theme.custom.fontFamily,
+    color: '#194563',
+    textTransform: 'uppercase',
 
+  },
+  tableBodyRow: {
+    borderSpacing: '0',
+    borderCollapse: 'collapse',
+    color: '#3E7AAA',
+  },
+  tableCell: {
+    fontFamily: theme.custom.fontFamily,
+    fontSize: '14px',
+    padding: '8px 15px 8px 0px',
+    borderBottom: '0.66px solid #087CA5',
+  },
+  headerCell: {
+    borderBottom: '4px solid #087CA5',
+    borderSpacing: '0',
+    borderCollapse: 'collapse',
+    fontWeight: 'bolder',
+  },
 });
 
 AboutBody.defaultProps = {
