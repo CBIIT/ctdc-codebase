88bf2760ae3c00a577bf970a7148dc0b6463d9b6
jonkiky <jonkiky@gmail.com>
2020-04-13 14:07:44 -0400

    update codacy

--- src/main/frontend/scripts/build.js
+++ src/main/frontend/scripts/build.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
@@ -117,7 +118,7 @@ checkBrowsers(paths.appPath, isInteractive)
       );
     },
     (err) => {
-      /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+     
       console.error(chalk.red('Failed to compile.\n'));
       printBuildError(err);
       process.exit(1);
--- src/main/frontend/src/serviceWorker.js
+++ src/main/frontend/src/serviceWorker.js
@@ -1,3 +1,6 @@
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
